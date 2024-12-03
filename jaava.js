d74df33fbcc9a4348bcd7b633c590b7f0b89aab5
gameradars <adarshgaming64@gmail.com>
2024-12-03 09:44:53 -0800

    Update jaava.js

--- jaava.js
+++ jaava.js
@@ -22,3 +22,44 @@ window.addEventListener("wheel",function(dets){
         })
     }
 })
+
+        const navbar = document.getElementById('navbar')
+        const closeMenu = document.getElementById('close')
+        const menu = document.getElementById('menu')
+
+        menu.addEventListener('click', () => {
+            navbar.classList.add('active')
+        })
+        closeMenu.addEventListener('click', () => {
+            navbar.classList.remove('active')
+        })
+
+
+        const skills = document.getElementById('skills')
+        const contact = document.getElementById('contact')
+
+
+        const skillsactive = () => {
+            console.log(window.scrollY + '>' + skills.offsetTop + skills.offsetHeight)
+            if (window.scrollY > skills.offsetTop) {
+                skills.classList.add('active')
+            }
+            if (window.scrollY > (contact.offsetTop)) {
+                skills.classList.remove('active')
+            }
+        }
+
+        window.addEventListener('scroll', skillsactive)
+
+
+        const header = document.getElementById('header')
+        const HeaderActive = () => {
+            if (window.scrollY >100) {
+                header.classList.add('active')
+            }else {
+                header.classList.remove('active')
+            }
+        }
+
+        window.addEventListener('scroll', HeaderActive)
+
