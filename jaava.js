8f8abce63bd1d430207af0d12707e8e6a2e38215
gameradars <adarshgaming64@gmail.com>
2025-11-24 21:31:50 +0545

    Update jaava.js

--- jaava.js
+++ jaava.js
@@ -1,65 +1,33 @@
-const hamburger = document.querySelector('.header .nav-bar .nav-list .hamburger');
-const mobile_menu = document.querySelector('.header .nav-bar .nav-list ul');
-const menu_item = document.querySelectorAll('.header .nav-bar .nav-list ul li a');
-const header = document.querySelector('.header.container');
+window.addEventListener("scroll",function(){
+    let header = document.querySelector("header")
+    header.classList.toggle('sticky' , window.scrollY > 0);
+})
 
-hamburger.addEventListener('click', () => {
-	hamburger.classList.toggle('active');
-	mobile_menu.classList.toggle('active');
-});
+let menubtn = document.querySelector(".menu-btn")
+let nav = document.querySelector(".nav")
+let navItems = document.querySelectorAll(".nav a")
 
-document.addEventListener('scroll', () => {
-	var scroll_position = window.scrollY;
-	if (scroll_position > 250) {
-		header.style.backgroundColor = '#29323c';
-	} else {
-		header.style.backgroundColor = 'transparent';
-	}
-});
+menubtn.addEventListener("click",function(){
+    menubtn.classList.toggle("active")
+    nav.classList.toggle("active")
+    closebtn.classList.toggle("active")
+})
 
-menu_item.forEach((item) => {
-	item.addEventListener('click', () => {
-		hamburger.classList.toggle('active');
-		mobile_menu.classList.toggle('active');
-	});
-});
-        const navbar = document.getElementById('navbar')
-        const closeMenu = document.getElementById('close')
-        const menu = document.getElementById('menu')
+navItems.forEach((navItem) =>{
+    navItem.addEventListener("click", function(){
+        menubtn.classList.remove("active")
+        nav.classList.remove("active")
+        closebtn.classList.remove("active")
+    })
+})
 
-        menu.addEventListener('click', () => {
-            navbar.classList.add('active')
-        })
-        closeMenu.addEventListener('click', () => {
-            navbar.classList.remove('active')
-        })
+let scrolltopbtn = document.querySelector(".scrolltop-btn")
 
+window.addEventListener("scroll",function(){
+    scrolltopbtn.classList.toggle("active", window.scrollY > 500)
+})
 
-        const skills = document.getElementById('skills')
-        const contact = document.getElementById('contact')
-
-
-        const skillsactive = () => {
-            console.log(window.scrollY + '>' + skills.offsetTop + skills.offsetHeight)
-            if (window.scrollY > skills.offsetTop) {
-                skills.classList.add('active')
-            }
-            if (window.scrollY > (contact.offsetTop)) {
-                skills.classList.remove('active')
-            }
-        }
-
-        window.addEventListener('scroll', skillsactive)
-
-
-        const header = document.getElementById('header')
-        const HeaderActive = () => {
-            if (window.scrollY >100) {
-                header.classList.add('active')
-            }else {
-                header.classList.remove('active')
-            }
-        }
-
-        window.addEventListener('scroll', HeaderActive)
-
+scrolltopbtn.addEventListener("click",function(){
+    document.body.scrollTop = 0;
+    document.documentElement.scrollTop = 0;
+})
